15156fbbe3221e56507f45da28a486cdff4898c7
ScubaScott <scott.reiling@gmail.com>
2016-03-28 11:31:45 -0500

    Added files via upload

---
+++ Arduino-Bluetooth.ino
@@ -0,0 +1,303 @@
+// UTouch_ButtonTest 
+// Copyright (C)2015 Rinky-Dink Electronics, Henning Karlsen. All right reserved
+// web: http://www.RinkyDinkElectronics.com/
+//
+// This program is a quick demo of how create and use buttons.
+//
+// This program requires the UTFT library.
+//
+// It is assumed that the display module is connected to an
+// appropriate shield or that you know how to change the pin 
+// numbers in the setup.
+//
+
+#include <UTFT.h>
+#include <UTouch.h>
+#include <Servo.h>
+#include <SPI.h>
+#include <boards.h>
+#include <RBL_nRF8001.h>
+
+
+// Initialize display
+// ------------------
+// Set the pins to the correct ones for your development board
+// -----------------------------------------------------------
+UTFT    myGLCD(ILI9325D_8,38,39,40,41);
+
+// Initialize touchscreen
+// ----------------------
+// Set the pins to the correct ones for your development board
+// -----------------------------------------------------------
+UTouch  myTouch( 6, 5, 4, 3, 2);
+
+// Declare which fonts we will be using
+extern uint8_t BigFont[];
+extern uint8_t SmallFont[];
+
+int x, y;
+char stCurrent[20]="";
+int stCurrentLen=0;
+char stLast[20]="";
+
+/*************************
+**   Custom functions   **
+*************************/
+
+void drawButtons()
+{
+    myGLCD.clrScr();
+// Draw the upper row of buttons
+  for (x=0; x<5; x++)
+  {
+    myGLCD.setColor(0, 0, 255);
+    myGLCD.fillRoundRect (10+(x*60), 10, 60+(x*60), 60);
+    myGLCD.setColor(255, 255, 255);
+    myGLCD.drawRoundRect (10+(x*60), 10, 60+(x*60), 60);
+    myGLCD.printNumI(x+1, 27+(x*60), 27);
+  }
+// Draw the center row of buttons
+  for (x=0; x<5; x++)
+  {
+    myGLCD.setColor(0, 0, 255);
+    myGLCD.fillRoundRect (10+(x*60), 70, 60+(x*60), 120);
+    myGLCD.setColor(255, 255, 255);
+    myGLCD.drawRoundRect (10+(x*60), 70, 60+(x*60), 120);
+    if (x<4)
+      myGLCD.printNumI(x+6, 27+(x*60), 87);
+  }
+  myGLCD.print("0", 267, 87);
+// Draw the lower row of buttons
+  myGLCD.setColor(0, 0, 255);
+  myGLCD.fillRoundRect (10, 130, 150, 180);
+  myGLCD.setColor(255, 255, 255);
+  myGLCD.drawRoundRect (10, 130, 150, 180);
+  myGLCD.print("Clear", 40, 147);
+  myGLCD.setColor(0, 0, 255);
+  myGLCD.fillRoundRect (160, 130, 300, 180);
+  myGLCD.setColor(255, 255, 255);
+  myGLCD.drawRoundRect (160, 130, 300, 180);
+  myGLCD.print("Enter", 190, 147);
+  myGLCD.setBackColor (0, 0, 0);
+}
+
+void updateStr(int val)
+{
+  if (stCurrentLen<20)
+  {
+    stCurrent[stCurrentLen]=val;
+    stCurrent[stCurrentLen+1]='\0';
+    stCurrentLen++;
+    myGLCD.setColor(0, 255, 0);
+    myGLCD.print(stCurrent, LEFT, 224);
+  }
+  else
+  {
+    myGLCD.setColor(255, 0, 0);
+    myGLCD.print("BUFFER FULL!", CENTER, 192);
+    delay(500);
+    myGLCD.print("            ", CENTER, 192);
+    delay(500);
+    myGLCD.print("BUFFER FULL!", CENTER, 192);
+    delay(500);
+    myGLCD.print("            ", CENTER, 192);
+    myGLCD.setColor(0, 255, 0);
+  }
+}
+
+// Draw a red frame while a button is touched
+void waitForIt(int x1, int y1, int x2, int y2)
+{
+  myGLCD.setColor(255, 0, 0);
+  myGLCD.drawRoundRect (x1, y1, x2, y2);
+  while (myTouch.dataAvailable())
+    myTouch.read();
+  myGLCD.setColor(255, 255, 255);
+  myGLCD.drawRoundRect (x1, y1, x2, y2);
+}
+
+static byte buf_len = 0;
+
+void ble_write_string(byte *bytes, uint8_t len)
+{
+  if (buf_len + len > 20)
+  {
+    for (int j = 0; j < 15000; j++)
+      ble_do_events();
+    
+    buf_len = 0;
+  }
+  
+  for (int j = 0; j < len; j++)
+  {
+    ble_write(bytes[j]);
+    buf_len++;
+  }
+    
+  if (buf_len == 20)
+  {
+    for (int j = 0; j < 15000; j++)
+      ble_do_events();
+    
+    buf_len = 0;
+  }  
+}
+
+/*************************
+**  Required functions  **
+*************************/
+
+void setup()
+{
+// Initial setup
+  Serial.begin(57600);
+  Serial.println("BLE Arduino Slave");
+  ble_begin();
+  
+  myGLCD.InitLCD();
+  myGLCD.clrScr();
+
+  myTouch.InitTouch();
+  myTouch.setPrecision(PREC_MEDIUM);
+
+  myGLCD.setFont(SmallFont);
+  myGLCD.setBackColor(0, 0, 255);
+  drawButtons();  
+}
+
+void loop()
+{
+  int buf[318];
+  int x, x2;
+  int y, y2;
+  int r;
+  while (true)
+  {
+    if (myTouch.dataAvailable())
+    {
+      myTouch.read();
+      x=myTouch.getX();
+      y=myTouch.getY();
+      
+      if ((y>=10) && (y<=60))  // Upper row
+      {
+        if ((x>=10) && (x<=60))  // Button: 1
+        {
+          waitForIt(10, 10, 60, 60);
+          updateStr('1');
+        }
+        if ((x>=70) && (x<=120))  // Button: 2
+        {
+          waitForIt(70, 10, 120, 60);
+          updateStr('2');
+        }
+        if ((x>=130) && (x<=180))  // Button: 3
+        {
+          waitForIt(130, 10, 180, 60);
+          updateStr('3');
+        }
+        if ((x>=190) && (x<=240))  // Button: 4
+        {
+          waitForIt(190, 10, 240, 60);
+          updateStr('4');
+        }
+        if ((x>=250) && (x<=300))  // Button: 5
+        {
+          waitForIt(250, 10, 300, 60);
+          updateStr('5');
+        }
+      }
+
+      if ((y>=70) && (y<=120))  // Center row
+      {
+        if ((x>=10) && (x<=60))  // Button: 6
+        {
+          waitForIt(10, 70, 60, 120);
+          updateStr('6');
+        }
+        if ((x>=70) && (x<=120))  // Button: 7
+        {
+          waitForIt(70, 70, 120, 120);
+          updateStr('7');
+        }
+        if ((x>=130) && (x<=180))  // Button: 8
+        {
+          waitForIt(130, 70, 180, 120);
+          updateStr('8');
+        }
+        if ((x>=190) && (x<=240))  // Button: 9
+        {
+          waitForIt(190, 70, 240, 120);
+          updateStr('9');
+        }
+        if ((x>=250) && (x<=300))  // Button: 0
+        {
+          waitForIt(250, 70, 300, 120);
+          updateStr('0');
+        }
+      }
+
+      if ((y>=130) && (y<=180))  // Upper row
+      {
+        if ((x>=10) && (x<=150))  // Button: Clear
+        {
+          waitForIt(10, 130, 150, 180);
+          stCurrent[0]='\0';
+          stCurrentLen=0;
+          myGLCD.setColor(0, 0, 0);
+          myGLCD.fillRect(0, 224, 319, 239);
+        }
+        if ((x>=160) && (x<=300))  // Button: Enter
+        {
+          waitForIt(160, 130, 300, 180);
+          if (stCurrentLen>0)
+          {
+            for (x=0; x<stCurrentLen+1; x++)
+            {
+              stLast[x]=stCurrent[x];
+            }
+            stCurrent[0]='\0';
+            stCurrentLen=0;
+            myGLCD.setColor(0, 0, 0);
+            
+            myGLCD.fillRect(0, 208, 319, 239);
+            myGLCD.setColor(0, 255, 0);
+            myGLCD.print(stLast, LEFT, 208);
+          }
+          else
+          {
+            // Clear the screen and draw the frame
+              myGLCD.clrScr();
+              drawButtons();
+          }
+        }
+      }
+    }
+    ble_do_events();
+   if ( ble_available() )
+  {
+    String Scmd = "";
+    while(ble_available())
+    {
+      byte cmd;
+      cmd = ble_read();
+      Scmd.concat(cmd);
+    }
+      // Parse data here
+      Serial.println("CMD:" + Scmd);
+      if (Scmd == "UNLOCK"){
+        Serial.println("Unlocking");
+        byte buf[] = {'D','O','N','E'};         
+        ble_write_string(buf, 4);   
+      }else
+      {
+        Serial.println("Unknown command");
+        byte buf[] = {'E','R','R','O','R'};         
+        ble_write_string(buf, 5);
+      }
+          // send out any outstanding data
+      buf_len = 0;
+      return; // only do this task in this loop
+  }
+}
+}
